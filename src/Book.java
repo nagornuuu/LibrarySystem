08331ff81b430d10e5970d8a7e3fa74574fd5dae
Andrii <a.nahornyi@be-tse.com>
2025-01-21 10:11:23 +0100

    Implemented functionalities of class Book

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,23 @@
+/**
+ * Class which represents a book
+ */
 public class Book {
+    private String name;
+    private String genre;
+
+    // Contructor which initialize book details
+    public Book (String name, String genre) {
+        this.name = name;
+        this.genre = genre;
+    }
+
+    // Getter which return book name
+    public String getName() {
+        return name;
+    }
+
+    // Getter which return book genre
+    public String getGenre() {
+        return genre;
+    }
 }
