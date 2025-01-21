6bf2fd80953f6cdca9b1f762591dc67f34ee1b2c
Andrii <a.nahornyi@be-tse.com>
2025-01-21 10:27:13 +0100

    Added fields, and method addBook

--- src/Library.java
+++ src/Library.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
+
 public class Library {
+    // constants
+    private static final int MAX_BOOKS = 5; // The number of maximum books in each genre
+    private static final int GENRES = 3;    // The Number of genres in a system
+
+    // Fields
+    private static Book[][] books = new Book[MAX_BOOKS][MAX_BOOKS];     // 2D array in which we store the books
+    private static String[] genres = {"Fiction", "Science", "History"}; // Array where we store genres
+    private static int[] bookAdded = new int[GENRES]; // thankfully to this variable , we can track the number of books added to genre
+    private static int totalBooks = 0;  // Track the number of books in the library
+
+    public static void addBook(Scanner scanner) {
+        System.out.println("\n Available Genres: ");
+        for (int i = 0; i < genres.length; i++) {
+            System.out.println((i + 1) + ". " + genres[i]); // display genre options
+        }
+
+        System.out.println("Select a Genre (1-" + GENRES + "): ");
+        int genreChoice = scanner.nextInt() - 1;
+        scanner.nextLine();
+
+        if (genreChoice < 0 || genreChoice > GENRES) {
+            System.out.println("Invalid Genre Choice. Try again");
+            return;
+        }
+
+        if (bookAdded[genreChoice] >= MAX_BOOKS) {
+            System.out.println("The genre is full of books. We cannot add more books");
+            return;
+        }
+
+        System.out.println("Enter the name of the book: ");
+        String bookName = scanner.nextLine();
+
+        books[genreChoice][bookAdded[genreChoice]] = new Book(bookName, genres[genreChoice]);
+        bookAdded[genreChoice]++;
+        totalBooks++;
+        System.out.println("Book added successfully");
+    }
 }
