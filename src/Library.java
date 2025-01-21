5ca2eae53dd969d7a954db90ef72cb3b24791750
Andrii <a.nahornyi@be-tse.com>
2025-01-21 11:05:51 +0100

    Change the display method, made it represents grapically

--- src/Library.java
+++ src/Library.java
@@ -41,17 +41,34 @@ public class Library {
         System.out.println("Book added successfully");
     }
 
+    // Method to display all books in the library graphically
     public static void displayBooks() {
-        System.out.println("\nLibrary Books: ");
+        System.out.println("\nLibrary Shelf:");
         for (int i = 0; i < genres.length; i++) {
-            System.out.println("Genre: " + genres[i]);
-            if (bookAdded[i] == 0) {
-                System.out.println(" No Books available");
-            } else {
-                for (int j = 0; j < bookAdded[i]; j++) {
-                    System.out.println("  - " + books[i][j].getName());
+            System.out.println("\nGenre: " + genres[i]); // Display genre name
+            System.out.print("+");
+            for (int k = 0; k < MAX_BOOKS; k++) {
+                System.out.print("-----+"); // Top border of the shelf
+            }
+            System.out.println();
+
+            // Display book slots for the current genre
+            System.out.print("|");
+            for (int j = 0; j < MAX_BOOKS; j++) {
+                if (j < bookAdded[i] && books[i][j] != null) {
+                    System.out.printf("%-5s|", books[i][j].getName().substring(0, Math.min(5, books[i][j].getName().length())));
+                } else {
+                    System.out.print("     |"); // Empty slot
                 }
             }
+            System.out.println();
+
+            // Bottom border of the shelf
+            System.out.print("+");
+            for (int k = 0; k < MAX_BOOKS; k++) {
+                System.out.print("-----+");
+            }
+            System.out.println();
         }
     }
 
